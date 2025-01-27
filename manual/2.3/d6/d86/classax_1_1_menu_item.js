553faf5c418c9ff548cc8469494d8d4dca8c1ba9
halx99 <halx99@users.noreply.github.com>
2025-01-27 15:27:24 +0000

    deploy: db737ae0b66a58ffb0288698096732e9066c51ab

---
+++ manual/2.3/d6/d86/classax_1_1_menu_item.js
@@ -0,0 +1,16 @@
+var classax_1_1_menu_item =
+[
+    [ "MenuItem", "d6/d86/classax_1_1_menu_item.html#a6f66011bf639313fa7e7d3145d65f752", null ],
+    [ "~MenuItem", "d6/d86/classax_1_1_menu_item.html#af824d27b2c5717fe6d169e25c5452595", null ],
+    [ "rect", "d6/d86/classax_1_1_menu_item.html#a16014f2aa404c91e422dc213f93617a8", null ],
+    [ "activate", "d6/d86/classax_1_1_menu_item.html#a44d98e5a5532610ba5a8a0a3cf513bd1", null ],
+    [ "selected", "d6/d86/classax_1_1_menu_item.html#a4ed8c9e3e442668ae47041794cf17ab0", null ],
+    [ "unselected", "d6/d86/classax_1_1_menu_item.html#ae940275b97aedcf7add1b31c93a79533", null ],
+    [ "isEnabled", "d6/d86/classax_1_1_menu_item.html#ae9ed4bd4596d96caecc0bb19e9dc4db5", null ],
+    [ "setEnabled", "d6/d86/classax_1_1_menu_item.html#a84f2bfd8d5e3a6b6c75dc8b0c0cb25f3", null ],
+    [ "isSelected", "d6/d86/classax_1_1_menu_item.html#a4913bb420196482ee2c0534807a9a36a", null ],
+    [ "setCallback", "d6/d86/classax_1_1_menu_item.html#a0b278f5ef665a38805b196152f0c290d", null ],
+    [ "setCallbackSelected", "d6/d86/classax_1_1_menu_item.html#a44b01d7ad54d6c6b37ad409ea0f516c3", null ],
+    [ "getDescription", "d6/d86/classax_1_1_menu_item.html#a52b7741f1ccd38d665e153882b7dc0dd", null ],
+    [ "initWithCallback", "d6/d86/classax_1_1_menu_item.html#a8ab8caf1c0569c5d1d04ed2612d25733", null ]
+];
---
+++ manual/2.3/navtreedata.js
@@ -0,0 +1,80 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "Axmol Engine", "index.html", [
+    [ "axmol", "index.html", "index" ],
+    [ "Modules", "modules.html", "modules" ],
+    [ "Namespace Members", "namespacemembers.html", [
+      [ "All", "namespacemembers.html", null ],
+      [ "Functions", "namespacemembers_func.html", null ],
+      [ "Variables", "namespacemembers_vars.html", null ],
+      [ "Typedefs", "namespacemembers_type.html", null ],
+      [ "Enumerations", "namespacemembers_enum.html", null ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", "functions_vars" ],
+        [ "Typedefs", "functions_type.html", null ],
+        [ "Enumerations", "functions_enum.html", null ],
+        [ "Enumerator", "functions_eval.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"annotated.html",
+"d0/de0/classax_1_1_vec3.html#ae610b67e30d286a4eb847304271c1c08",
+"d2/d1f/classax_1_1_label.html",
+"d2/d9b/classax_1_1_action_ease.html#a1306e29fda504409b52d24e2002fbee3",
+"d3/d5c/classax_1_1_image.html#ab4e88c89b3b7ea1735996cc4def22d58a92769fe7c40229f4301d6125e0a9e928",
+"d3/dbc/classax_1_1network_1_1_http_response.html#a6f1638a033fc2ecb57c8d1f4124d4f14",
+"d4/d72/classax_1_1_director.html#add0fe462d1d8f825ebf654cd688c4d36",
+"d5/d3f/classax_1_1_animation.html#a9d88104c3cb0e3047802780ed779f752",
+"d6/d78/classax_1_1_action_tween.html#ab16555b33c5b1584507908cc7a5cd755",
+"d7/d3d/classax_1_1_place.html#a96e22ef872245449fc1e189b17d751af",
+"d8/d39/classax_1_1_particle_system.html#a19f5613efb62e2454a9d953e240c13d7",
+"d8/d6e/classax_1_1_mat4.html#a89e2e181668f2487d80b4ffcb4abaa47",
+"d9/d3b/classax_1_1_motion_streak3_d.html#a0f59730fa0bd109f318e0a18bb4f8945",
+"da/d07/classax_1_1_audio_profile.html#a48a64cce118fbff6c57c718629383b92",
+"da/dc2/classax_1_1backend_1_1_texture2_d_g_l.html#a9d80d413fdd3f3320e67e83c92143111",
+"db/dbf/classax_1_1backend_1_1_buffer_g_l.html#a8d5947535170d55bfc1db3b7d239c6f8",
+"dc/da6/classax_1_1_draw_node.html#a3ae2589002479d1f0cf3ff7a49c47259",
+"dd/d3e/classax_1_1ui_1_1_slider.html#acc62db38aaf6cc3e189f6fe47d9dcbf8",
+"dd/df8/classax_1_1ui_1_1_list_view.html#aadfc86f35c88f9cfe40d671277e4a955",
+"de/dd7/classax_1_1_audio_decoder_wav.html#afed100fa54a9a67fe17ff7f172300849",
+"df/da2/classax_1_1_node.html#a5df40ff931bfdfac2b9af3c103fa21c1",
+"index.html#sec1"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
